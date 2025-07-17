9558954ce57fa559a75e4b914eba19504ce4fc72
tolgahan özden <tolgahanozden@hotmail.com>
2025-07-17 13:52:10 +0300

    aaa

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from app import app
+from waitress import serve
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+     #app.run(host='0.0.0.0', port=5011, debug=True, use_reloader=False)
+     print("Starting the application with Waitress...")
+     serve(app, host='0.0.0.0', port=5011)
